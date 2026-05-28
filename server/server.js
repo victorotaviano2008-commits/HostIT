6a4ac4e5a4e1e7c9dd25f799ffc13f47bed1574c
victorotaviano2008-commits <victorotaviano2008@gmail.com>
2026-05-28 12:04:29 -0300

    Client

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 
 const corsOptions = {
   origin: (origin, callback) => {
-    const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173'];
+    const allowedOrigins = ['http://localhost:3000', process.env.FRONTEND_URL];
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
