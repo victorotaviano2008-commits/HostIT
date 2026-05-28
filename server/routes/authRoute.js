7d90900094c6219adeb2efdb9be86ff9a4d555c9
victorotaviano2008-commits <victorotaviano2008@gmail.com>
2026-05-28 11:46:10 -0300

    Fix: convert server submodule to regular folder on Fedora

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -9,13 +9,3 @@ router.get('/verify-reset-token', authControllers.verifyResetToken);
 router.post('/reset-password', authControllers.resetPassword);
 
 module.exports = router;const authControllers = require('../controllers/authController');
-const express = require('express');
-const router = express.Router();
-
-router.post('/register', authControllers.register);
-router.post('/login', authControllers.login);
-router.post('/forgot-password', authControllers.forgotPassword);
-router.get('/verify-reset-token', authControllers.verifyResetToken);
-router.post('/reset-password', authControllers.resetPassword);
-
-module.exports = router;
